7c0cc52c09db4017b98560e125688a55adb55143
RuikkaAnton <anton.ruikka@gmail.com>
2024-04-30 12:16:13 +0300

    made arm and note easier to see and made cabinet openable

---
+++ Assets/Scripts/OpenCabinet.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCabinet : MonoBehaviour
+{
+    [SerializeField] GameObject Cabinet;
+    [SerializeField] GameObject cabinetUI;
+    [SerializeField] LayerMask maskCabinet;
+
+    public GameObject CabinetOpen;
+
+    Camera cam;
+    public bool isDoorOpening = false;
+    bool isDoorMoving = false;
+    float smoothTime = 4f;
+
+    Vector3 targetPosition;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        targetPosition = Cabinet.transform.position;
+    }
+
+    private void Update()
+    {
+        if (!isDoorMoving)
+        {
+            OpenDoor(maskCabinet, cam, cabinetUI, Cabinet);
+        }
+        if (isDoorOpening)
+        {
+            Cabinet.transform.position = Vector3.Lerp(Cabinet.transform.position, targetPosition, smoothTime * Time.deltaTime);
+            if (Vector3.Distance(Cabinet.transform.position, targetPosition) < 0.1f)
+            {
+                Cabinet.transform.position = targetPosition;
+                isDoorOpening = false;
+                isDoorMoving = false;
+            }
+        }
+    }
+
+    private void OpenDoor(LayerMask mask, Camera cam, GameObject UI, GameObject item)
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 2, mask))
+        {
+            if (hit.collider.gameObject == item)
+            {
+                UI.SetActive(true);
+            }
+        }
+        else
+        {
+            UI.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && !isDoorMoving)
+        {
+            if (Physics.Raycast(ray, out hit, 2, mask))
+            {
+                if (CabinetOpen.activeSelf)
+                {
+                    targetPosition = Cabinet.transform.position + new Vector3(-0.5f, 0, 0);
+                    CabinetOpen.SetActive(false);
+                }
+                else
+                {
+                    targetPosition = Cabinet.transform.position + new Vector3(0.5f, 0, 0);
+                    CabinetOpen.SetActive(true);
+                }
+                isDoorOpening = true;
+                isDoorMoving = true;
+            }
+        }
+    }
+}
