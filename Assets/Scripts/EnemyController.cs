128f6c938b68af5c55c533d61a9807db54bda9ca
AleKsi37 <aleksilaukkanen9@gmail.com>
2024-05-14 13:01:45 +0300

    Enemy attack animations

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,11 +8,45 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public Transform Player;
-    
-    private void Update()
+    public Transform player;
+    private Animator animator;
+    public float attackRange = 2f;
+
+    private bool isAttacking = false;
+
+    private void Start()
     {
-        agent.SetDestination(Player.position);
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= attackRange)
+        {
+            if (!isAttacking)
+            {
+                agent.isStopped = true;
+                animator.SetTrigger("Attack");
+                isAttacking = true;
+            }
+            else
+            {
+                
+                agent.SetDestination(transform.position);
+            }
+        }
+        else
+        {
+            if (isAttacking)
+            {
+                agent.isStopped = false;
+                isAttacking = false;
+            }
+
+            agent.SetDestination(player.position);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
