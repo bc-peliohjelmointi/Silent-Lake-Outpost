05f115ef27895bcf984582e6b3872f94fcae507e
RuikkaAnton <anton.ruikka@gmail.com>
2024-05-15 13:01:56 +0300

    added barrier so even if player has no chance of survival  and ui gets disabled when game iver screen appears

--- Assets/Scripts/ActivateCannibal.cs
+++ Assets/Scripts/ActivateCannibal.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class ActivateCannibal : MonoBehaviour
 {
     [SerializeField] GameObject cannibal;
+    [SerializeField] GameObject Barrier;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ActivateCannibal"))
         {
+            Barrier.SetActive(true);
             cannibal.SetActive(true);
         }
     }
